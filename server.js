836051a478372fa413c330f8dd34acb4aadd84e3
maikalangi <maikalangi@gmail.com>
2022-05-17 06:11:16 -0700

    fixes return to root path on 0 bottles path

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const port = 3000
 app.get('/', (req, res)=>{
     res.send(`99 Bottles of beer on the wall <br/>
     <a href="/98">take one down, pass it around</a>`);
+    console.log(location.pathname);
 });
 
 app.get('/:bottleNumbers', (req, res)=>{
-    res.send(`${req.params.bottleNumbers} Bottles of beer on the wall <br/>
-    <a href=/${req.params.bottleNumbers-1}>take one down, pass it around</a>`);
+    if (req.params.bottleNumbers == 0){
+        res.send(`req.params.bottleNumbers Bottles of beer on the wall <br/>
+        <a href=/>Now put them all back on the wall</a>`);
+    } else {
+        res.send(`${req.params.bottleNumbers} Bottles of beer on the wall <br/>
+        <a href=${req.params.bottleNumbers-1}>take one down, pass it around</a>`);
+    }
 });
 
 app.listen(port, ()=>{
